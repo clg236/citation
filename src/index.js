797593713a8e49e251b6f67c928cffc85979a13c
Christian Grewell <christian@nyu.edu>
2021-06-06 17:01:01 -0400

    centering

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Unity, { UnityContext } from "react-unity-webgl";
 import { createGlobalStyle } from 'styled-components'
 import { AspectRatio } from 'react-aspect-ratio';
+import styled from 'styled-components';
 import 'react-aspect-ratio/aspect-ratio.css'
 
 const GlobalStyle = createGlobalStyle`
@@ -13,6 +14,16 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+const GameContainer = styled.div`
+  display: flex;
+  justify-content:center; // centers in the flex direction and the default flex-direction is row
+  align-items:center; // centers perpendicular to the flex direction
+  height: 100vh; // 100% view height
+  width: 100vw; // 100% view width
+  position: absolute; // so it goes behind the current content
+  background-color: black;
+`
+
 const unityContext = new UnityContext({
   loaderUrl: "build/Builds.loader.js",
   dataUrl: "build/Builds.data",
@@ -21,7 +32,7 @@ const unityContext = new UnityContext({
 })
 
 const Game = () => (
-  <AspectRatio ratio="3/4" style={{ maxWidth: '800px' }}>
+  <AspectRatio ratio="9/16" style={{ minWidth: '360px' }}>
     <Unity unityContext={unityContext} devicePixelRatio={2} />;
   </AspectRatio>
 );
@@ -30,7 +41,9 @@ function App() {
   return (
     <React.Fragment>
       <GlobalStyle />
-      <Game />
+      <GameContainer>
+        <Game />
+      </GameContainer>
     </React.Fragment>
   )
 }
